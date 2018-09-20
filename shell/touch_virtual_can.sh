b6dba42aaae8f7d1558d809bc1a53f85bb0b5120
Cherish <cherishgww@gmail.com>
2018-09-21 00:36:08 +0800

    script: touch virtual can add param

--- shell/touch_virtual_can.sh
+++ shell/touch_virtual_can.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 # Bring up a virtual can bus device
+
+if [ "$1" != "" ]; then
+    IFACE=$1
+else
+    IFACE="vcan0"
+fi
+
 modprobe can
 modprobe can_raw
+modprobe can_bcm
 modprobe vcan
 
-ip link add dev vcan0 type vcan
-ip link set up vcan0
+ip link add dev $IFACE type vcan
+ip link set up $IFACE
+
+ifconfig $IFACE up
