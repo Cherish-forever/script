4b3aa54e0ba3487311b1f093303eb2222cef2ded
Cherish <851153978@qq.com>
2021-04-11 03:55:50 +0800

    fix issue: Fix can untils install depends

--- vcan/install_can-untils.sh
+++ vcan/install_can-untils.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # install can-utils
+sudo apt -y install libtool autoconf
 git clone https://github.com/linux-can/can-utils.git /tmp/can-utils
 cd /tmp/can-utils && ./autogen.sh && ./configure && make -j8 && sudo make install
