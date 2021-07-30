092bcbacfab5914d0f49414c0319a91e27e1a3a5
Cherish <851153978@qq.com>
2021-07-30 23:08:51 +0800

    feature: Add some kicad plugin

--- kicad/kicad.sh
+++ kicad/kicad.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-echo "Install kicad 5.1"
-sudo add-apt-repository --yes ppa:js-reynaud/kicad-5.1
-sudo apt-get update
-sudo apt -y install --install-suggests kicad
-sudo apt -y install kicad-locale-zh
-
-echo "Download kicad library"
-mkdir ~/kicad_lib
-git clone https://github.com/KiCad/kicad-symbols.git ~/kicad_lib/kicad-symbols
-git clone https://github.com/KiCad/kicad-footprints.git ~/kicad_lib/kicad-footprints
-git clone https://github.com/KiCad/kicad-packages3D.git ~/kicad_lib/kicad-packages3D
-git clone https://github.com/KiCad/kicad-templates.git ~/kicad_lib/kicad-templates
-
+#echo "Install kicad 5.1"
+#sudo add-apt-repository --yes ppa:js-reynaud/kicad-5.1
+#sudo apt-get update
+#sudo apt -y install --install-suggests kicad
+#sudo apt -y install kicad-locale-zh
+#
+#echo "Download kicad library"
+#mkdir ~/kicad_lib
+#git clone https://github.com/KiCad/kicad-symbols.git ~/kicad_lib/kicad-symbols
+#git clone https://github.com/KiCad/kicad-footprints.git ~/kicad_lib/kicad-footprints
+#git clone https://github.com/KiCad/kicad-packages3D.git ~/kicad_lib/kicad-packages3D
+#git clone https://github.com/KiCad/kicad-templates.git ~/kicad_lib/kicad-templates
+#
 mkdir -p ~/.kicad_plugins
 
 echo "Download kicad stepup tools"
@@ -58,8 +58,8 @@ echo "or cd ~/.kicad_plugins/altium2kicad && sudo make install"
 echo "Download kicad schematics diff"
 sudo apt -y install libgtk-3-dev libcairo2-dev libgit2-dev transfig imagemagick make
 sudo apt -y install build-essential
-git clone https://gitlab.com/neo900/eeshow.git ~/kicad_lib/plugins/eeshow
-cd ~/kicad_lib/plugins/eeshow && make -j4 && sudo make install && cd -
+git clone https://gitlab.com/neo900/eeshow.git ~/.kicad_plugins/eeshow
+cd ~/.kicad_plugins/eeshow && make -j4 && sudo make install && cd -
 echo "eeshow help:"
 echo "$ eeplot -o neo900.pdf neo900.pro"
 echo "$ eediff -o diff.png -s 2 6a9f71:neo900.pro neo900_SS_5.sch neo900.pro neo900_SS_5.sch"
@@ -70,3 +70,18 @@ pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 pip3 install kicost
 echo "kicost help:"
 echo "Please read: https://xess.com/KiCost/docs/_build/singlehtml/index.html"
+
+echo "Download kicad round tracks"
+git clone https://github.com/stimulu/kicad-round-tracks.git ~/.kicad_plugins/kicad-round-tracks
+
+echo "Download kicad panelize"
+git clone https://github.com/msvisser/panelize-plugin.git ~/.kicad_plugins/panelize-plugin
+
+echo "Download kicad kikit"
+pip3 install kikit
+
+echo "Download eagle-to-kicad"
+git clone https://github.com/lachlanA/eagle-to-kicad.git ~/.kicad_plugins/eagle-to-kicad
+
+echi "Download kicad-footprint-generator"
+git clone https://gitlab.com/kicad/libraries/kicad-footprint-generator.git ~/.kicad_plugins/kicad-footprint-generator
